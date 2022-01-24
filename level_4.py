cc78acf9f6090cc587a788b05381ce246c6d306c
Your Name <sheracore@shera.com>
2022-01-25 00:34:24 +0330

    level 4 is near to solve

--- level_4.py
+++ level_4.py
@@ -4,13 +4,12 @@ import requests
 response = requests.get("http://www.pythonchallenge.com/pc/def/equality.html")
 context = response.content # This context is byte type
 context = str(context).replace(r'\n','') # Converting to str type and remove all \n
-print(context)
+#print(context)
 
- regex patterns
-result_pattern = re.compile(r"\w")
-
-main_string = string_pattern.findall(context)[0]
-result = result_pattern.findall(main_string)
-print(''.join(result))
-
-
+#regex patterns
+#result_pattern = re.compile(r"([A-Z])\1\1([a-z])([A-Z])\1\1")
+#result_pattern = re.compile(r"([A-Z])\1([a-z])([A-Z])\1")
+#result_pattern = re.compile(r"([A-Z])\1\1([a-z])")
+result_pattern = re.compile(r"[a-z][A-Z][A-Z][A-Z]([a-z])[A-Z][A-Z][A-Z][a-z]")
+result = result_pattern.findall(context)
+print("".join(result))
