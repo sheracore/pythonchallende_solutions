d4db33854617569b47857c10bb20510876d5b0b4
Your Name <sheracore@shera.com>
2022-01-29 09:43:19 +0330

    Add swap to switch_number_simple_list script

--- pythonchallenges/switch_number_simple_list.py
+++ pythonchallenges/switch_number_simple_list.py
@@ -3,8 +3,6 @@ print(list_)
 
 for index in range(len(list_)-1):
     if index%2 != 0: continue
-    tmp = list_[index]
-    list_[index] = list_[index+1]
-    list_[index+1] = tmp
+    list_[index], list_[index+1] = list_[index+1], list_[index]
 
 print(list_)
