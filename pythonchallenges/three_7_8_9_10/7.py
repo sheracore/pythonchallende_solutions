d35405a8ff04609c972c27999248076df1867965
Your Name <sheracore@shera.com>
2022-01-30 22:43:12 +0330

    Add three_7_8_9_10 number 7

---
+++ pythonchallenges/three_7_8_9_10/7.py
@@ -0,0 +1,19 @@
+class DoubleSum():
+
+    def __init__(self, initial=0):
+        self.initial = initial
+
+    def double_sum(self, list_):
+        print(list_)
+        result = list()
+        next_ = 0
+        for value in list_:
+            next_ += value
+            result.append(next_ + self.initial)
+
+        return(result)
+
+
+obj = DoubleSum(initial=100)
+list_ = [1,2,3,4,5]
+print(obj.double_sum(list_))
